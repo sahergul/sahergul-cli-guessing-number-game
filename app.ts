6dee3213d7ac5c226626d822f6bac4f0fe59b9d6
sahergul <sahergul91@gmail.com>
2024-07-08 22:06:17 +0500

    created cli guessing game

---
+++ app.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+// CLI guessing number game
+
+const randomnumber = Math.floor(Math.random()* 6+1)
+
+console.log("welcome to CLI guessing number Game");
+
+let answer = await inquirer.prompt({
+            name: "userguessednumber",
+            message: "please guess number between 1-6:",
+            type: "number"
+});
+
+if (answer.userguessednumber === randomnumber){
+    console.log("congratulation!, you guessed the right number")
+}
+else {console.log("you guessed the wrong number")}
